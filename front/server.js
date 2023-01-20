fd0e10357d313886beb2bfaae2ccf4ce01306e12
baek <baekbr13@gmail.com>
2023-01-20 15:19:24 +0900

    feat: 페이지 구성

---
+++ front/server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const cookieParser = require("cookie-parser")
+const nunjucks = require("nunjucks")
+const router = require('./routs')
+
+
+
+app.set("view engine", "html")
+nunjucks.configure("views", {
+    express:app,
+})
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(express.static("public"))
+
+app.use(router)
+
+app.listen(3005, async()=>{
+    console.log("front server open")
+})
