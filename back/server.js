69a97a499140a4b156d8618209891886b2a8c8ea
cloudcoke <lms4296@gmail.com>
2023-01-20 14:14:14 +0900

    feat: model 생성

--- back/server.js
+++ back/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const app = require("./app");
+const { sequelize } = require("./models");
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
+  await sequelize.sync({ force: true });
   console.log(`Server Start`);
 });
